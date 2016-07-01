0ee0ca45cfd2640f7502da727be1cb4e21aaa36b
Bui Men <buithanhmen@gmail.com>
2016-07-01 16:32:18 +0700

    Notify

---
+++ notify1.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Notify</title>
+	<link rel="stylesheet" href="/css/reset.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/font-pacifico.css">
+	<link rel="stylesheet" href="css/font-helvetica-neue-medium.css">
+	<link rel="stylesheet" href="css/font-helvetica-neue.css">
+	<link rel="stylesheet" href="css/font-raleway-semiBold.css">
+	<link rel="stylesheet" href="css/font-raleway-bold.css">
+	<link rel="stylesheet" href="css/style1.css">
+	<link rel="stylesheet" href="css/reponsive.css">
+	<script src="https://use.fontawesome.com/c50c5bf797.js"></script>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="header-top">
+			</div>
+			<div class="header-main">
+				<div class="col-sm-6 col-sm-offset-2">
+					<div class="notify">
+						<span>Notify</span>
+						<span class="color">.</span>
+					</div>
+					<p>A great new free psd theme to showcase your new application.</p>
+					<div class="icons">
+						<div class="circle">
+							<a href=""><i class="fa fa-apple" aria-hidden="true"></i></a>
+						</div>
+						<div class="circle">
+							<a href=""><i class="fa fa-android" aria-hidden="true"></i></a>
+						</div>
+						<div class="color circle">
+							<a href=""><i class="fa fa-windows" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="content">
+			<div class="content-top">
+				<div class="col-sm-4">
+					<div class="circle">
+						<a href=""><i class="fa fa-cog" aria-hidden="true"></i></i></a>
+					</div>
+					<div class="center">
+						<span>Editable Theme</span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="circle">
+						<a href=""><i class="fa fa-star" aria-hidden="true"></i></a>
+					</div>
+					<div class="center">
+						<span>Flat Design</span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="circle">
+						<a href=""><i class="fa fa-globe" aria-hidden="true"></i></a>
+					</div>
+					<div class="center">
+						<span>Reach Your Audience</span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+					</div>
+				</div>
+			</div>
+			<div class="content-main">
+				<div class="main-top">
+					<div class="col-sm-6">
+						<span>Get Notified Of Any Updates!</span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla nisl congue congue. Maecenas nec condimentum libero, at elementum mauris. Phasellus eget nisi dapibus, ultricies nisl at, hendrerit risusuis ornare luctus id sollicitudin ante lobortis at.</p>
+						<div class="input-group">
+					      	<input type="text" class="form-control" placeholder="Email Address">
+					      	<div class="input-group-addon">Notify</div>
+    					</div>
+					</div>
+					<div class="col-sm-6">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/AQm9GDjVdgs" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+			<div class="content-bottom">
+				<div class="bottom-1">
+					<p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum velit vitae nulla molestie eu commodo nulla dapib us. Donec id sollicitudin urna. Integer at dui ac leo fermentum varius eleifend ut.”</p>
+					<footer>Jane Hunt</footer>
+				</div>
+				<div class="slide">
+					<!-- <marquee> -->
+						<?php
+							for ($i = 1; $i <=21; $i++) {
+						?>
+							<a href="#"><img src="images/slide-<?php echo $i; ?>.png" title="Ghi chú cho ảnh"/> </a>
+						<?php
+							}
+						?>
+					<!-- </marquee> -->
+				</div>
+				<div class="bottom-2">
+					<footer>Say Hi & Get in Touch</footer>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse.</p>
+				</div>
+				<div class="icons">
+					<div class="circle">
+						<a href=""><i class="fa fa-twitter" aria-hidden="true"></i></i></a>
+					</div>
+					<div class="circle">
+						<a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					</div>
+					<div class="color circle">
+						<a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+					</div>
+					<div class="color circle">
+						<a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+					</div>
+					<div class="color circle">
+						<a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+					</div>
+					<div class="color circle">
+						<a href=""><i class="fa fa-youtube" aria-hidden="true"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			<ul>
+				<li><a href="">Contact</a></li>
+				<li><a href="">Download</a></li>
+				<li><a href="">Press</a></li>
+				<li><a href="">Email</a></li>
+				<li><a href="">Support</a></li>
+				<li><a href="">Privacy Policy</a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
