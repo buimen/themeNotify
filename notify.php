0c5f1aaeff005bbe96017d032a92799b9166543d
Bui Men <buithanhmen@gmail.com>
2016-07-04 17:04:47 +0700

    themeNotify

--- notify.php
+++ notify.php
@@ -22,66 +22,73 @@
 		<div class="header">
 			<div class="header-top">
 			</div>
-			<div class="header-main">
-				<div class="notify">
-					<span>Notify</span>
-					<span class="color">.</span>
-				</div>
-				<p>A great new free psd theme to showcase your new application.</p>
-				<div class="icons">
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-apple" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
+			<div class="header-main" tabindex="-1">
+				<div class="header-text container">
+					<div class="row">
+						<div class="notify col-sm-6">
+							<span>Notify</span>
+							<span class="color">.</span>
+						</div>
 					</div>
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-android" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
+					<div class="row">
+						<p class="col-sm-6">A great new free psd theme to showcase your new application.</p>
 					</div>
-					<div class="color circle">
-						<a href="">
-							<i class="fa fa-windows" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
+					<div class="row">
+						<div class="icons col-sm-6">
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-apple" aria-hidden="true"></i>
+								</a>
+							</div>
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-android" aria-hidden="true"></i>
+								</a>
+							</div>
+							<div class="color circle">
+								<a href="">
+									<i class="fa fa-windows" aria-hidden="true"></i>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="content">
 			<div class="content-top">
-				<div class="col-sm-4">
-					<div class="circle">
-						<a href=""><i class="fa fa-cog" aria-hidden="true"></i></i></a>
+				<div class="container">
+					<div class="col-sm-4">
+						<div class="circle">
+							<a href=""><i class="fa fa-cog" aria-hidden="true"></i></i></a>
+						</div>
+						<div class="center">
+							<span>Editable Theme</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+						</div>
 					</div>
-					<div class="center">
-						<span>Editable Theme</span>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+					<div class="col-sm-4">
+						<div class="circle">
+							<a href=""><i class="fa fa-star" aria-hidden="true"></i></a>
+						</div>
+						<div class="center">
+							<span>Flat Design</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+						</div>
 					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="circle">
-						<a href=""><i class="fa fa-star" aria-hidden="true"></i></a>
-					</div>
-					<div class="center">
-						<span>Flat Design</span>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="circle">
-						<a href=""><i class="fa fa-globe" aria-hidden="true"></i></a>
-					</div>
-					<div class="center">
-						<span>Reach Your Audience</span>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+					<div class="col-sm-4">
+						<div class="circle">
+							<a href=""><i class="fa fa-globe" aria-hidden="true"></i></a>
+						</div>
+						<div class="center">
+							<span>Reach Your Audience</span>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+						</div>
 					</div>
 				</div>
 			</div>
 			<div class="content-main">
-				<div class="main-top">
+				<div class="container">
 					<div class="col-sm-6">
 						<span>Get Notified Of Any Updates!</span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla nisl congue congue. Maecenas nec condimentum libero, at elementum mauris. Phasellus eget nisi dapibus, ultricies nisl at, hendrerit risusuis ornare luctus id sollicitudin ante lobortis at.</p>
@@ -90,98 +97,77 @@
 					      	<div class="input-group-addon">Notify</div>
     					</div>
 					</div>
-					<div class="col-sm-6">
+					<div class="col-sm-5 col-sm-offset-1">
 						<iframe src="https://www.youtube.com/embed/AQm9GDjVdgs" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
 			</div>
 			<div class="content-bottom">
-				<div class="bottom-1">
-					<p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum velit vitae nulla molestie eu commodo nulla dapib us. Donec id sollicitudin urna. Integer at dui ac leo fermentum varius eleifend ut.”</p>
-					<footer>Jane Hunt</footer>
-				</div>
-				<div class="slide">
-					<!-- <marquee> -->
-						<?php
-							for ($i = 1; $i <=10; $i++) {
-						?>
-							<div class="slide-img">
-								<a href="#">
-									<img class="users" src="images/slide-<?php echo $i; ?>.png" title="Ghi chú cho ảnh"/>
-									<img class="layer" src="images/layer.png">
-									<img class="hover" src="images/click.png">
+				<div class="container">
+					<div class="bottom-1">
+						<p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum velit vitae nulla molestie eu commodo nulla dapib us. Donec id sollicitudin urna. Integer at dui ac leo fermentum varius eleifend ut.”</p>
+						<footer>Jane Hunt</footer>
+					</div>
+					<div class="slide col-xs-12 col-sm-10 col-sm-offset-2 col-md-8 col-md-offset-3">
+						<!-- <marquee> -->
+							<?php
+								for ($i = 1; $i <=20; $i++) {
+							?>
+								<div class="slide-img">
+									<a href="#">
+										<img class="users" src="images/slide-<?php echo $i; ?>.png" title="Ghi chú cho ảnh"/>
+										<img class="layer" src="images/layer.png">
+									</a>
+								</div>
+							<?php
+								}
+							?>
+						<!-- </marquee> -->
+					</div>
+					<div class="bottom-2 col-xs-12">
+						<footer>Say Hi & Get in Touch</footer>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse.</p>
+					</div>
+					<div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-lg-6 col-lg-offset-4">
+						<div class="icons">
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-twitter" aria-hidden="true"></i>
 								</a>
 							</div>
-						<?php
-							}
-							for ($i = 11; $i <=20; $i++) {
-						?>
-							<div class="slide-img hide-img">
-								<a href="#">
-									<img class="users" src="images/slide-<?php echo $i; ?>.png" title="Ghi chú cho ảnh"/>
-									<img class="layer" src="images/layer.png">
-									<img class="hover" src="images/click.png">
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-facebook" aria-hidden="true"></i>
 								</a>
 							</div>
-						<?php
-							}
-						?>
-							<div class="slide-img">
-								<a href="#">
-									<img class="users" src="images/slide-21.png" title="Ghi chú cho ảnh"/>
-									<img class="layer" src="images/layer.png">
-									<img class="hover" src="images/click.png">
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-pinterest" aria-hidden="true"></i>
 								</a>
 							</div>
-					<!-- </marquee> -->
-				</div>
-				<div class="bottom-2">
-					<footer>Say Hi & Get in Touch</footer>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse.</p>
-				</div>
-				<div class="icons">
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-twitter" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
-					</div>
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-facebook" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
-					</div>
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-pinterest" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
-					</div>
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-google-plus" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
-					</div>
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-linkedin" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
-					</div>
-					<div class="circle">
-						<a href="">
-							<i class="fa fa-youtube" aria-hidden="true"></i>
-							<img class="hover" src="images/click.png">
-						</a>
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-google-plus" aria-hidden="true"></i>
+								</a>
+							</div>
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-linkedin" aria-hidden="true"></i>
+								</a>
+							</div>
+							<div class="circle">
+								<a href="">
+									<i class="fa fa-youtube" aria-hidden="true"></i>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="footer">
 			<div class="container menu">
-				<ul>
+				<ul class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-7 col-lg-offset-3">
 					<li><a href="">Contact</a></li>
 					<li><a href="">Download</a></li>
 					<li><a href="">Press</a></li>
